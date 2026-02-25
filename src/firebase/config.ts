7bbcdd0273dc430ebd8de7932a445881e5e93a36
Wei Chen <cwei84480@gmail.com>
2026-02-25 15:55:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7668388905-7972a",
+  "appId": "1:1092960264730:web:f575c0951dd3796173e7a3",
+  "apiKey": "AIzaSyAYpWTzDwYyOwndN-rYjicAi1Su99pfki4",
+  "authDomain": "studio-7668388905-7972a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1092960264730"
+};
